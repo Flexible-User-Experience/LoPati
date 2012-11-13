6ae3a05f71222bbed30d6e80e14aac2e78af801c
Marc Morales <marcmorales83@gmail.com>
2012-11-13 20:07:54 +0100

    mini calendari 4

--- src/LoPati/BlogBundle/Repository/PaginaRepository.php
+++ src/LoPati/BlogBundle/Repository/PaginaRepository.php
@@ -33,7 +33,7 @@ class PaginaRepository extends EntityRepository
     {
         $query = $this->getEntityManager()
             ->createQuery('SELECT n FROM BlogBundle:Pagina n WHERE n.actiu = 1 AND
-             (n.startDate>=:inici AND n.startDate<=:fi OR n.endDate>=:inici AND n.startDate<=:fi OR n.startDate<=:inici AND n.endDate>=:fi)
+             (n.startDate>=:inici AND n.startDate<=:fi OR n.endDate>=:inici AND n.endDate<=:fi OR n.startDate<=:inici AND n.endDate>=:fi)
              AND n.categoria IS NOT NULL AND n.subCategoria IS NOT NULL ORDER BY n.startDate ASC');
         $query->setParameter('inici', date('Y-m-d', mktime(0, 0, 0, $mes, 1, $any)));
         $query->setParameter('fi', date('Y-m-t', mktime(0, 0, 0, $mes, 28, $any)));    // la opcion -t devuelve la cantidad de dias que tiene el mes dado
@@ -52,14 +52,18 @@ class PaginaRepository extends EntityRepository
         $query = $this->getEntityManager()
             ->createQuery('SELECT n FROM BlogBundle:Pagina n WHERE n.actiu = 1 AND n.startDate >= :inici ORDER BY n.endDate ASC');
         $query->setParameter('inici', date('Y-m-d', mktime(0, 0, 0, $mes, 1, $any)));
+
+
         return $query->getResult();
     }
 
     public function getActiveItemsFromDayAndMonthAndYear($dia,$mes, $any)
     {
+        	$newDate = date("Y-m-d", strtotime($any.'-'.$mes.'-'.$dia));
         $query = $this->getEntityManager()
             ->createQuery('SELECT n FROM BlogBundle:Pagina n WHERE n.actiu = 1 AND n.startDate <= :avui AND n.endDate >= :avui ORDER BY n.endDate ASC');
-        $query->setParameter('avui', date('Y-m-d', mktime(0, 0, 0, $mes, $dia, $any)));
+        //$query->setParameter('avui', date('Y-m-d', mktime(0, 0, 0, $mes, $dia, $any)));
+        $query->setParameter('avui', $newDate);
            // la opcion -t devuelve la cantidad de dias que tiene el mes dado
         return $query->getResult();
     }
