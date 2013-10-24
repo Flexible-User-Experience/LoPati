3720ce488973a212ac5b84fe6cde0ca5a991ce1e
David Romaní <david@flux.cat>
2013-10-24 11:18:07 +0200

    newsletter save users command improved - step 6.6

--- src/LoPati/NewsletterBundle/Command/NewsletterSaveUsersCommand.php
+++ src/LoPati/NewsletterBundle/Command/NewsletterSaveUsersCommand.php
@@ -1,5 +1,6 @@
 <?php
 namespace LoPati\NewsletterBundle\Command;
+
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
@@ -10,93 +11,69 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use LoPati\NewsletterBundle\Entity\NewsletterUser;
 
 class NewsletterSaveUsersCommand extends ContainerAwareCommand {
-	protected function configure() {
+
+	protected function configure() 
+    {
 		$this->setName('newsletter:save:users')
-				->setDefinition(
-						array(
-								new InputArgument('fitxer',
-										InputArgument::OPTIONAL,
-										'fitxer'
-										),))
+				->setDefinition(array(new InputArgument('fitxer', InputArgument::REQUIRED, 'fitxer')))
 				->setDescription('Arxiu amb correus')
 				->setHelp(
-						<<<EOT
+<<<EOT
 La comanda <info>newsltter:ssave:users</info> importa usuaris a la base de dades.
 EOT
 				);
 	}
 
-	protected function execute(InputInterface $input, OutputInterface $output) {
-		
+	protected function execute(InputInterface $input, OutputInterface $output) 
+    {	
+        $output->writeln('Guardant mails....');
 		$contenedor = $this->getContainer();
 		$em = $contenedor->get('doctrine')->getEntityManager();
 		
 		$max = $input->getArgument('fitxer');
-
-		$filas=file($max);
-		
-		
+		$filas = file($max);	
 		$handle = fopen($max, 'r');
-				
-		$i=0;
-		$numero_fila=0;
-		
-        $z=0;
+		$i = 0;
+		$numero_fila = 0;
+        $z = 0;
 
 		// mientras exista una fila
-		while (!feof($handle)){
+		while (!feof($handle)) {
 
-		$row=fgets($handle, 4096);
+            $row = fgets($handle, 4096);
             //$row=stream_get_line($handle, 4096,'\r');
 			// genero array con por medio del separador "," que es el que tiene el archivo txt
-            $sql=str_replace(" ", "", $row);
-            $sql=str_replace(",", "", $sql);
-            $sql=str_replace("\n", "", $sql);
-            $sql=str_replace("\r", "", $sql);
+            $sql = str_replace(" ", "", $row);
+            $sql = str_replace(",", "", $sql);
+            $sql = str_replace("\n", "", $sql);
+            $sql = str_replace("\r", "", $sql);
 
-			$query = $em
-			->createQuery(
-					'SELECT u FROM NewsletterBundle:NewsletterUser u  WHERE
-					u.email = :mail');
+			$query = $em->createQuery('SELECT u FROM NewsletterBundle:NewsletterUser u WHERE u.email = :mail');
 			$query->setParameter('mail', $sql);
-			
 			$query->setMaxResults('1');
 			$existeix = $query->getResult();
 			
-						if (count($existeix)>0){
-
-
-                            $output->writeln("No s'ha pogut afegir el email: ".$sql);
-                            $z++;
-
-
-
-						}else{
-
-                            if (strlen($sql)){
-                            $user= new newsletterUser();
-                            $user->setEmail($sql);
-                            $user->setActive('1');
-                            $user->setIdioma('ca');
-                            $em->persist($user);
-
-                            $output->writeln("S'ha afegit un registre nou amb email: ".$sql);
-
-                            $i++;
-                                $em->flush();
-                            }
-                            $numero_fila++;
-
-
-
-                        }
+			if (count($existeix) > 0) {
+                $output->writeln("<error>No s'ha pogut afegir el email " . $sql . " perquè ja existeix a la base de dades</error>");
+                $z++;
+			} else {
+                if (strlen($sql)) {
+                    $user = new newsletterUser();
+                    $user->setEmail($sql);
+                    $user->setActive('1');
+                    $user->setIdioma('ca');
+                    $em->persist($user);
+                    $output->writeln("<info>S'ha afegit un registre nou a la base de dades amb el email " . $sql . "</info>");
+                    $i++;
+                    $em->flush();
+                }
+            }
+            $numero_fila++;
 			
 		}
-		$output->writeln('Guardant mails....');
-
-        $output->writeln($z. ' mails fallats');
-		$output->writeln($i. ' mails guardats');
-
+        $output->writeln($z . ' mails fallats');
+		$output->writeln($i . ' mails guardats');
+        $output->writeln('TOTAL ' . $numero_fila . ' mails avaluats');
 	}
 }
 
