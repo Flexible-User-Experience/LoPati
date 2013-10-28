bd637177973f89b14b86e02664486638644dc534
David Romaní <david@flux.cat>
2013-10-28 12:13:20 +0100

    pre insert of irradiador project inside menu render - step 6.7

--- src/LoPati/MenuBundle/Controller/DefaultController.php
+++ src/LoPati/MenuBundle/Controller/DefaultController.php
@@ -7,10 +7,9 @@ use LoPati\BlogBundle\Entity;
 
 class DefaultController extends Controller {
 
-	public function indexAction($name) {
-		return $this
-				->render('MenuBundle:Default:index.html.twig',
-						array('name' => $name));
+	public function indexAction($name)
+    {
+		return $this->render('MenuBundle:Default:index.html.twig',	array('name' => $name));
 	}
 
 	public function pintaMenuAction($id=null, $pagina=null)
@@ -21,20 +20,33 @@ class DefaultController extends Controller {
 			$pagina = $em->getRepository('BlogBundle:Pagina')->find($id);
 		}
 		$categories = $em->getRepository('MenuBundle:Categoria')->findCategories();
-		return $this->render('MenuBundle:Default:menu.html.twig', array('categories' => $categories, 'id' => $id, 'pagina' => $pagina));
+
+		return $this->render('MenuBundle:Default:menu.html.twig', array(
+            'categories' => $categories,
+            'id' => $id,
+            'pagina' => $pagina,
+        ));
 	}
 	
 	public function creaLinkAction($idPagina, $tipus, $actiu, $titol, $llista)
     {
 		if ($llista == 1) {
-			$pagina=null;
+			$pagina = null;
 		} else {
 			$em = $this->getDoctrine()->getManager();
 			$pagina = $em->getRepository('BlogBundle:Pagina')->find($idPagina);
 			$logger = $this->get('logger');
 			$logger->info('id val:'.$idPagina);
 		}
-		return $this->render('MenuBundle:Default:creaLink.html.twig', array('pagina' => $pagina, 'id' => $idPagina, 'tipus' => $tipus, 'actiu' => $actiu, 'titol' => $titol, 'llista' => $llista));
+
+		return $this->render('MenuBundle:Default:creaLink.html.twig', array(
+            'pagina' => $pagina,
+            'id' => $idPagina,
+            'tipus' => $tipus,
+            'actiu' => $actiu,
+            'titol' => $titol,
+            'llista' => $llista,
+        ));
 	}
 	
 	public function subCategoriesAction($idPagina)
@@ -42,16 +54,17 @@ class DefaultController extends Controller {
 		$em = $this->getDoctrine()->getManager();
 		$pagina = $em->getRepository('BlogBundle:Pagina')->findOneBy(array('id' => $idPagina));
 		$categoria = $pagina->getCategoria();
-		
 		/*$logger = $this->get('logger');
 		$logger->info('id val:'.$categoria);*/
-
 		$subcategories2 = $em->getRepository('MenuBundle:SubCategoria')->findSubCategories($categoria);
 		
-		return $this->render('MenuBundle:Default:subCategories.html.twig',array('subcategories' => $subcategories2, 'id' => $idPagina));
+		return $this->render('MenuBundle:Default:subCategories.html.twig', array(
+            'subcategories' => $subcategories2,
+            'id' => $idPagina,
+        ));
 	}
 	
-	public function pintaMenuLlistaAction($categoria_id, $subcategoria_id=null, $onlycategories=null)
+	public function pintaMenuLlistaAction($categoria_id, $subcategoria_id = null, $onlycategories = null)
     {
 		$em = $this->getDoctrine()->getManager();
 		$categories = $em->getRepository('MenuBundle:Categoria')->findCategories();
@@ -59,8 +72,13 @@ class DefaultController extends Controller {
 		$em = $this->getDoctrine()->getManager();
 		$subcategories = $em->getRepository('MenuBundle:SubCategoria')->findSubCategories($categoria_id);
 
-		return $this->render('MenuBundle:Default:menuLlista.html.twig', array('categories' => $categories, 'categoria_id' => $categoria_id,'subcategoria_id' => $subcategoria_id, 'subcategories' => $subcategories, 'onlycategories' => $onlycategories));
-	
+		return $this->render('MenuBundle:Default:menuLlista.html.twig', array(
+            'categories' => $categories,
+            'categoria_id' => $categoria_id,
+            'subcategoria_id' => $subcategoria_id,
+            'subcategories' => $subcategories,
+            'onlycategories' => $onlycategories,
+        ));
 	}
 
 }
