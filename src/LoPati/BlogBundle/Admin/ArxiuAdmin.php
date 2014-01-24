0993f9f76de2bc2b8cf23f7a4b139fe16b3a231b
David Romaní <david@flux.cat>
2014-01-24 11:17:53 +0100

    export actions refactor

--- src/LoPati/BlogBundle/Admin/ArxiuAdmin.php
+++ src/LoPati/BlogBundle/Admin/ArxiuAdmin.php
@@ -1,57 +1,73 @@
 <?php
 
-
 namespace LoPati\BlogBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
-use Sonata\AdminBundle\Show\ShowMapper;
-use Sonata\PageBundle\Model\PageInterface;
-
-
-use Knp\Menu\ItemInterface as MenuItemInterface;
-
+use Sonata\AdminBundle\Route\RouteCollection;
 
 class ArxiuAdmin extends Admin
 {
-	
+    protected $datagridValues = array(
+        '_page'       => 1,
+        '_sort_order' => 'DESC', // sort direction
+        '_sort_by'    => 'any' // field name
+    );
 
-	protected function configureFormFields(FormMapper $formMapper)
-	{
-		$formMapper
-		->add('any', 'integer',array('label' => 'Any'))
-		->add('actiu','checkbox',array('required'=>FALSE))
-		->add('imagePetita', 'file', array('label' => 'Imatge any', 'required'=>false))
-		->add('imagePetitaName', 'text', array('label' => 'Nom', 'required' => false, 'read_only'=>true,))
-		->add('imagePetita2', 'file', array('label' => 'Imatge any vermell', 'required'=>false))
-		->add('imagePetita2Name', 'text', array('label' => 'Nom', 'required' => false, 'read_only'=>true,))
-		
-		->setHelps(array('any'=>'Ex: 2012'))
-		;
-	}
-	
-	protected function configureListFields(ListMapper $mapper)
-	{
-		$mapper
-		//->addIdentifier('id')
-		->addIdentifier('any')
-		->add('imagePetitaName', null, array('label' => 'Imatge any'))
-		->add('imagePetita2Name', null, array('label' => 'Imatge any vermell'))
-		->add('_action', 'actions', array(
-				'actions' => array(
-						//'view' => array(),
-						'edit' => array(),
-						//'delete' => array(),
-				), 'label' => 'Accions'));
-	}
+    /**
+     * Configure export formats
+     *
+     * @return array
+     */
+    public function getExportFormats()
+    {
+        return array('xls', 'csv');
+    }
 
-	protected $datagridValues = array(
-			'_page' => 1,
-			'_sort_order' => 'DESC', // sort direction
-			'_sort_by' => 'any' // field name
-	);
-	
-	
+    /**
+     * Configure route collection
+     *
+     * @param RouteCollection $collection collection
+     *
+     * @return mixed
+     */
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('delete');
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('any', 'integer', array('label' => 'Any'))
+            ->add('actiu', 'checkbox', array('required' => false))
+            ->add('imagePetita', 'file', array('label' => 'Imatge any', 'required' => false))
+            ->add('imagePetitaName', 'text', array('label' => 'Nom', 'required' => false, 'read_only' => true,))
+            ->add('imagePetita2', 'file', array('label' => 'Imatge any vermell', 'required' => false))
+            ->add('imagePetita2Name', 'text', array('label' => 'Nom', 'required' => false, 'read_only' => true,))
+
+            ->setHelps(array('any' => 'Ex: 2012'));
+    }
+
+    protected function configureListFields(ListMapper $mapper)
+    {
+        $mapper
+            //->addIdentifier('id')
+            ->addIdentifier('any')
+            ->add('imagePetitaName', null, array('label' => 'Imatge any'))
+            ->add('imagePetita2Name', null, array('label' => 'Imatge any vermell'))
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'actions' => array(
+                        //'view' => array(),
+                        'edit' => array(),
+                        //'delete' => array(),
+                    ),
+                    'label'   => 'Accions'
+                )
+            );
+    }
 }
--- src/LoPati/NewsletterBundle/Admin/NewsletterUserAdmin.php
+++ src/LoPati/NewsletterBundle/Admin/NewsletterUserAdmin.php
@@ -5,50 +5,72 @@ use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
-
 use Sonata\AdminBundle\Route\RouteCollection;
 
 class NewsletterUserAdmin extends Admin
 {
-	
-	protected function configureFormFields(FormMapper $formMapper)
-	{
-		$formMapper
-	
-		->add('email')
-		->add('idioma', 'choice', array( 'choices'   => array( 'ca' => 'Català','es' => 'Castellano','en'=>'English'), 'required'  => true,))
-		->add('active',null,array('label'=>'Actiu','required'=>false))
-		//->add('fail')
-		
-		
-		;
-	}
-	
-	protected function configureListFields(ListMapper $mapper)
-	{
-		$mapper
-		->add('id')
-		->addIdentifier('email')
-		->add('idioma')
-		->add('active',null,array('label'=>'Actiu'))
-		->add('token')
-		->add('fail')
-		->add('created',null,array('label'=>'Data alta'))
-		;
-	}
-	protected $datagridValues = array(
-			'_page' => 1,
-			'_sort_order' => 'ASC', // sort direction
-			'_sort_by' => 'email' // field name
-	);
+    protected $datagridValues = array(
+        '_page'       => 1,
+        '_sort_order' => 'ASC', // sort direction
+        '_sort_by'    => 'email' // field name
+    );
 
-	protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    /**
+     * Configure export formats
+     *
+     * @return array
+     */
+    public function getExportFormats()
+    {
+        return array('xls', 'csv');
+    }
+
+    /**
+     * Configure route collection
+     *
+     * @param RouteCollection $collection collection
+     *
+     * @return mixed
+     */
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('delete');
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+
+            ->add('email')
+            ->add(
+                'idioma',
+                'choice',
+                array(
+                    'choices'  => array('ca' => 'Català', 'es' => 'Castellano', 'en' => 'English'),
+                    'required' => true,
+                )
+            )
+            ->add('active', null, array('label' => 'Actiu', 'required' => false))//->add('fail')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $mapper)
+    {
+        $mapper
+            ->add('id')
+            ->addIdentifier('email')
+            ->add('idioma')
+            ->add('active', null, array('label' => 'Actiu'))
+            ->add('token')
+            ->add('fail')
+            ->add('created', null, array('label' => 'Data alta'));
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
             ->add('email')
-            ->add('active',null, array('label'=>'Actiu'))
-            ->add('created',null, array('label'=>'Data Alta'))
-        ;
+            ->add('active', null, array('label' => 'Actiu'))
+            ->add('created', null, array('label' => 'Data Alta'));
     }
-	
 }
