2f0b9951b01f77e982ffa4f14fc8950654ef4a05
David Romaní <david@flux.cat>
2014-10-10 12:49:11 +0200

    improve sonata admin UI 3

--- src/LoPati/BlogBundle/Entity/Arxiu.php
+++ src/LoPati/BlogBundle/Entity/Arxiu.php
@@ -13,137 +13,144 @@ use Gedmo\Mapping\Annotation as Gedmo;
  * @ORM\HasLifecycleCallbacks
  * @Vich\Uploadable
  */
-class Arxiu {
-	
-	/**
-	 * @ORM\Id
-	 * @ORM\Column(type="integer")
-	 * @ORM\GeneratedValue
-	 */
-	protected $id;
-	
-	
-	/**
-	 *  @ORM\Column(type="decimal", precision=4, unique=true) 
-	 */
-	protected $any = null;
-	
-	
-	/** @ORM\Column(type="boolean", nullable=true) */
-	protected $actiu = FALSE;
-	
-	/**
-	 * @Assert\File(
-	 *     maxSize="1M",
-	 *     mimeTypes={"image/png", "image/jpeg", "image/pjpeg"}
-	 * )
-	 * @Vich\UploadableField(mapping="imatge", fileNameProperty="imagePetitaName")
-	 *
-	 * @var File $imagePetita
-	 */
-	protected $imagePetita;
-	
-	/**
-	 * @ORM\Column(type="string", length=255, name="image_petita_name", nullable=true)
-	 *
-	 * @var string $imagePetitaName
-	 */
-	protected $imagePetitaName;
-	
-	/**
-	 * @Assert\File(
-	 *     maxSize="1M",
-	 *     mimeTypes={"image/png", "image/jpeg", "image/pjpeg"}
-	 * )
-	 * @Vich\UploadableField(mapping="imatge", fileNameProperty="imagePetita2Name")
-	 *
-	 * @var File $imagePetita2
-	 */
-	protected $imagePetita2;
-	
-	/**
-	 * @ORM\Column(type="string", length=255, name="image_petita2_name", nullable=true)
-	 *
-	 * @var string $imagePetita2Name
-	 */
-	protected $imagePetita2Name;
-	
-	/**
-	 * Get id
-	 * @return integer
-	 */
-	public function getId() {
-		return $this->id;
-	}
-	public function getImagePetitaName() {
-		return $this->imagePetitaName;
-	}
-	
-	public function setImagePetitaName($filename) {
-		$this->imagePetitaName=$filename;
-	}
-	
-	public function getImagePetita() {
-		return $this->imagePetita;
-	}
-	
-	public function setImagePetita($file) {
-		$this->imagePetita=$file;
-	}
-	
-	
-	public function getImagePetita2() {
-		return $this->imagePetita2;
-	}
-	
-	public function setImagePetita2($file) {
-		$this->imagePetita2=$file;
-	}
-	
-	public function getImagePetita2Name() {
-		return $this->imagePetita2Name;
-	}
-	
-	public function setImagePetita2Name($filename) {
-		$this->imagePetita2Name=$filename;
-	}
-	
-	
-	public function setAny($dataCaducitat) {
-		$this->any = $dataCaducitat;
-	}
-	
-	/**
-	 * Get data_caducitat
-	 *
-	 * @return date
-	 */
-	public function getAny() {
-		return $this->any;
-	}
-	
-	
-	/**
-	 * Set actiu
-	 *
-	 * @param boolean $actiu
-	 */
-	public function setActiu($actiu) {
-		$this->actiu = $actiu;
-	}
-	
-	/**
-	 * Get actiu
-	 *
-	 * @return boolean
-	 */
-	public function getActiu() {
-		return $this->actiu;
-	}
-	
-	public function __toString(){
-		
-		return $this->getAny();
-	}
-	
+class Arxiu
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="decimal", precision=4, unique=true)
+     */
+    protected $any = null;
+
+    /** @ORM\Column(type="boolean", nullable=true) */
+    protected $actiu = false;
+
+    /**
+     * @Assert\File(
+     *     maxSize="2M",
+     *     mimeTypes={"image/png", "image/jpeg", "image/pjpeg"}
+     * )
+     * @Vich\UploadableField(mapping="imatge", fileNameProperty="imagePetitaName")
+     *
+     * @var File $imagePetita
+     */
+    protected $imagePetita;
+
+    /**
+     * @ORM\Column(type="string", length=255, name="image_petita_name", nullable=true)
+     * @var string $imagePetitaName
+     */
+    protected $imagePetitaName;
+
+    /**
+     * @Assert\File(
+     *     maxSize="1M",
+     *     mimeTypes={"image/png", "image/jpeg", "image/pjpeg"}
+     * )
+     * @Vich\UploadableField(mapping="imatge", fileNameProperty="imagePetita2Name")
+     *
+     * @var File $imagePetita2
+     */
+    protected $imagePetita2;
+
+    /**
+     * @ORM\Column(type="string", length=255, name="image_petita2_name", nullable=true)
+     * @var string $imagePetita2Name
+     */
+    protected $imagePetita2Name;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getImagePetitaName()
+    {
+        return $this->imagePetitaName;
+    }
+
+    public function setImagePetitaName($filename)
+    {
+        $this->imagePetitaName = $filename;
+    }
+
+    public function getImagePetita()
+    {
+        return $this->imagePetita;
+    }
+
+    public function setImagePetita($file)
+    {
+        $this->imagePetita = $file;
+    }
+
+    public function getImagePetita2()
+    {
+        return $this->imagePetita2;
+    }
+
+    public function setImagePetita2($file)
+    {
+        $this->imagePetita2 = $file;
+    }
+
+    public function getImagePetita2Name()
+    {
+        return $this->imagePetita2Name;
+    }
+
+    public function setImagePetita2Name($filename)
+    {
+        $this->imagePetita2Name = $filename;
+    }
+
+    /**
+     * Get actiu
+     *
+     * @return boolean
+     */
+    public function getActiu()
+    {
+        return $this->actiu;
+    }
+
+    /**
+     * Set actiu
+     *
+     * @param boolean $actiu
+     */
+    public function setActiu($actiu)
+    {
+        $this->actiu = $actiu;
+    }
+
+    public function __toString()
+    {
+        return $this->getAny();
+    }
+
+    /**
+     * Get data_caducitat
+     *
+     * @return \DateTime
+     */
+    public function getAny()
+    {
+        return $this->any;
+    }
+
+    public function setAny($dataCaducitat)
+    {
+        $this->any = $dataCaducitat;
+    }
 }
