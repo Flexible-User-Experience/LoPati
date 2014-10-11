48c100e3ebc0256007288f4f1f43e9ff8654f4d0
David Romaní <david@flux.cat>
2014-10-11 19:52:49 +0200

    improve test suite 1

--- src/LoPati/BlogBundle/Tests/Controller/DefaultControllerTest.php
+++ src/LoPati/BlogBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,19 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testHomepage()
+    public function testInici()
     {
         $client = static::createClient();
         $client->request('GET', '/');
-
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
     }
 
-    public function testHomepageCa()
+    public function testPortada()
     {
         $client = static::createClient();
         $client->request('GET', '/ca/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $client->request('GET', '/es/');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $client->request('GET', '/en/');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $client->request('GET', '/xx/');
+        $this->assertTrue($client->getResponse()->isNotFound());
     }
 }
