b54a680b6657b78cbe0b52b9b26cdfd883883db7
David Romaní <david@flux.cat>
2014-11-17 11:50:52 +0100

    improve admin test suite

--- src/LoPati/BlogBundle/Entity/Translation/ConfiguracioTranslation.php
+++ src/LoPati/BlogBundle/Entity/Translation/ConfiguracioTranslation.php
@@ -8,7 +8,7 @@ use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
 /**
  * @ORM\Entity
  * @ORM\Table(name="configuracio_translations",
- *   uniqueConstraints={@ORM\UniqueConstraint(name="lookup_unique_idx", columns={
+ *   uniqueConstraints={@ORM\UniqueConstraint(name="lookup_configuration_unique_idx", columns={
  * 	"locale", "object_id", "field"
  *   })}
  * )
--- src/LoPati/MenuBundle/Entity/Translation/SubCategoriaTranslation.php
+++ src/LoPati/MenuBundle/Entity/Translation/SubCategoriaTranslation.php
@@ -8,7 +8,7 @@ use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
 /**
  * @ORM\Entity
  * @ORM\Table(name="subcategoria_translations",
- *   uniqueConstraints={@ORM\UniqueConstraint(name="lookup_unique_idx", columns={
+ *   uniqueConstraints={@ORM\UniqueConstraint(name="lookup_subcategory_unique_idx", columns={
  * 	"locale", "object_id", "field"
  *   })}
  * )
