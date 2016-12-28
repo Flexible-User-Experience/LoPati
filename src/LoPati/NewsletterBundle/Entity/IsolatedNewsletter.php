29c17f2f52d45d8609fe6dc19a10235d1f7abbcd
David Romaní <david@flux.cat>
2016-12-28 17:37:20 +0100

    add isolated newsletter entity

---
+++ src/LoPati/NewsletterBundle/Entity/IsolatedNewsletter.php
@@ -0,0 +1,240 @@
+<?php
+
+namespace LoPati\NewsletterBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class IsolatedNewsletter
+ *
+ * @category Entity
+ * @package  LoPati\NewsletterBundle\Entity
+ * @author   David Romaní <david@flux.cat>
+ *
+ * @ORM\Table(name="isolated_newsletter")
+ * @ORM\Entity(repositoryClass="LoPati\NewsletterBundle\Repository\IsolatedNewsletterRepository")
+ */
+class IsolatedNewsletter
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $subject;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="date", nullable=false)
+     */
+    private $date;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="integer", nullable=false)
+     */
+    private $state = 0;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(type="boolean", nullable=false)
+     */
+    private $tested = false;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $beginSend = null;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $endSend = null;
+
+    /**
+     * @var array|ArrayCollection
+     */
+    private $posts;
+
+    /**
+     *
+     *
+     * Methods
+     *
+     *
+     */
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param string $subject
+     *
+     * @return $this
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param \DateTime $date
+     *
+     * @return $this
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param int $state
+     *
+     * @return $this
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isTested()
+    {
+        return $this->tested;
+    }
+
+    /**
+     * @param bool $tested
+     *
+     * @return $this
+     */
+    public function setTested($tested)
+    {
+        $this->tested = $tested;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getBeginSend()
+    {
+        return $this->beginSend;
+    }
+
+    /**
+     * @param \DateTime $beginSend
+     *
+     * @return $this
+     */
+    public function setBeginSend($beginSend)
+    {
+        $this->beginSend = $beginSend;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getEndSend()
+    {
+        return $this->endSend;
+    }
+
+    /**
+     * @param \DateTime $endSend
+     *
+     * @return $this
+     */
+    public function setEndSend($endSend)
+    {
+        $this->endSend = $endSend;
+
+        return $this;
+    }
+
+    /**
+     * @return array|ArrayCollection
+     */
+    public function getPosts()
+    {
+        return $this->posts;
+    }
+
+    /**
+     * @param array|ArrayCollection $posts
+     *
+     * @return $this
+     */
+    public function setPosts($posts)
+    {
+        $this->posts = $posts;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->id ? $this->getBeginSend()->format('d/m/Y') . ' · ' . $this->subject : '---';
+    }
+}
