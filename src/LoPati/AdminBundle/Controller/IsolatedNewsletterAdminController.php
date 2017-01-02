8b79b4edf569ccff5b8d51064e102f866c605599
David Romaní <david@flux.cat>
2017-01-02 17:04:28 +0100

    improve isolated newsletter preview 2

--- src/LoPati/AdminBundle/Controller/IsolatedNewsletterAdminController.php
+++ src/LoPati/AdminBundle/Controller/IsolatedNewsletterAdminController.php
@@ -84,7 +84,13 @@ class IsolatedNewsletterAdminController extends Controller
             throw $this->createNotFoundException(sprintf('Unable to find isolated newsletter record with ID:%s', $id));
         }
 
-        return $this->render('AdminBundle:IsolatedNewsletter:preview.html.twig', array('newsletter' => $object));
+        return $this->render(
+            'AdminBundle:IsolatedNewsletter:preview.html.twig',
+            array(
+                'newsletter'   => $object,
+                'show_top_bar' => true,
+            )
+        );
     }
 
     /**
