869d40adc5e8658d7d9608d74985674d93ca2901
David Romaní <david@flux.cat>
2017-01-04 13:04:00 +0100

    improve dashboard sonata block upload xls 2

--- src/LoPati/AdminBundle/Admin/Block/IsolatedNewsletterXlsImportBlock.php
+++ src/LoPati/AdminBundle/Admin/Block/IsolatedNewsletterXlsImportBlock.php
@@ -5,6 +5,7 @@ namespace LoPati\AdminBundle\Admin\Block;
 use LoPati\AdminBundle\Form\Type\IsolatedNewsletterXlsFileUploadFormType;
 use Sonata\BlockBundle\Block\Service\AbstractBlockService;
 use Sonata\BlockBundle\Block\BlockContextInterface;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -25,6 +26,11 @@ class IsolatedNewsletterXlsImportBlock extends AbstractBlockService
     private $ff;
 
     /**
+     * @var Router
+     */
+    private $rs;
+
+    /**
      *
      *
      * Methods
@@ -38,11 +44,13 @@ class IsolatedNewsletterXlsImportBlock extends AbstractBlockService
      * @param string          $name
      * @param EngineInterface $templating
      * @param FormFactory     $formFactory
+     * @param Router          $router
      */
-    public function __construct($name, EngineInterface $templating, FormFactory $formFactory)
+    public function __construct($name, EngineInterface $templating, FormFactory $formFactory, Router $router)
     {
         parent::__construct($name, $templating);
         $this->ff = $formFactory;
+        $this->rs = $router;
     }
 
     /**
@@ -55,7 +63,13 @@ class IsolatedNewsletterXlsImportBlock extends AbstractBlockService
      */
     public function execute(BlockContextInterface $blockContext, Response $response = null)
     {
-        $form = $this->ff->create(IsolatedNewsletterXlsFileUploadFormType::class, null, []);
+        $form = $this->ff->create(
+            IsolatedNewsletterXlsFileUploadFormType::class,
+            null,
+            array(
+                'action' => $this->rs->generate('sonata_admin_dashboard'),
+            )
+        );
 
         return $this->renderResponse(
             $blockContext->getTemplate(),
