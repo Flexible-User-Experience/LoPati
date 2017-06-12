9524db7d87cc1110dfd24bb55acba34447ef13c2
David Romaní <david@flux.cat>
2017-06-12 17:04:10 +0200

    improve daily cronjob

--- src/LoPati/BlogBundle/Command/BlogChangeCategoriaCommand.php
+++ src/LoPati/BlogBundle/Command/BlogChangeCategoriaCommand.php
@@ -5,44 +5,47 @@ namespace LoPati\BlogBundle\Command;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Query;
 use LoPati\BlogBundle\Entity\Pagina;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Output\Output;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
+/**
+ * Class BlogChangeCategoriaCommand.
+ */
 class BlogChangeCategoriaCommand extends ContainerAwareCommand
 {
+    /**
+     * Configure.
+     */
     protected function configure()
     {
         $this->setName('blog:change:categoria')
             ->setDescription('assigna categoria Arxiu a pagines caducades')
             ->setHelp(
-                <<<EOT
+                <<<'EOT'
 La comanda <info>blog:change:categoria</info> assigna categoria Arxiu a pagines caducades.
 EOT
             );
     }
 
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $contenedor = $this->getContainer();
         /** @var EntityManager $em */
         $em = $contenedor->get('doctrine')->getManager();
         /** @var Query $query */
-        $query = $em
-            ->createQuery(
-                'SELECT p FROM BlogBundle:Pagina p JOIN p.categoria cat LEFT JOIN p.subCategoria sub  WHERE
-             p.data_caducitat <= :avui AND p.data_caducitat IS NOT NULL and p.actiu = TRUE'
+        $query = $em->createQuery(
+                'SELECT p FROM BlogBundle:Pagina p JOIN p.categoria cat LEFT JOIN p.subCategoria sub WHERE p.data_caducitat <= :today AND p.data_caducitat IS NOT NULL and p.actiu = TRUE'
             );
-        $query->setParameter('avui', new \DateTime('today'));
+        $query->setParameter('today', new \DateTime('today'));
         $pagines = $query->getResult();
-        $query = $em
-            ->createQuery(
-                'SELECT c FROM MenuBundle:Categoria c WHERE
-                c.nom = :cat'
-            );
+        $query = $em->createQuery('SELECT c FROM MenuBundle:Categoria c WHERE c.nom = :cat');
         $query->setParameter('cat', 'Arxiu');
         $categoria = $query->getSingleResult();
         /** @var Pagina $pagina */
@@ -50,7 +53,6 @@ EOT
             //$output->writeln('canvi estat:' . count($newsletter));
             $pagina->setCategoria($categoria);
             $pagina->setSubcategoria(null);
-            $em->persist($pagina);
         }
         $em->flush();
     }
