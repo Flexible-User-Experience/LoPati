37250e84c9ca77318dbaa5db2493511a1d76404a
David Romaní <david@flux.cat>
2018-05-31 16:17:56 +0200

    vendors update

--- src/LoPati/BlogBundle/Tests/Controller/DefaultControllerTest.php
+++ src/LoPati/BlogBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,8 +4,14 @@ namespace LoPati\BlogBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class DefaultControllerTest
+ */
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * Test homepage
+     */
     public function testInici()
     {
         $client = static::createClient();
@@ -13,6 +19,9 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
     }
 
+    /**
+     * Test locale homepage
+     */
     public function testPortada()
     {
         $client = static::createClient();
@@ -26,6 +35,9 @@ class DefaultControllerTest extends WebTestCase
         $this->assertTrue($client->getResponse()->isNotFound());
     }
 
+    /**
+     * Test categories
+     */
     public function testCategoriaEnllas()
     {
         $client = static::createClient();
