f45e2dcebf6b7743f5d5be0d4f79969800c36bd1
David Romaní <david@flux.cat>
2020-12-22 19:07:55 +0100

    remove CheckDNS from URL NewsletterPost

--- src/LoPati/NewsletterBundle/Entity/IsolatedNewsletterPost.php
+++ src/LoPati/NewsletterBundle/Entity/IsolatedNewsletterPost.php
@@ -83,7 +83,7 @@ class IsolatedNewsletterPost
      * @var string
      *
      * @ORM\Column(type="string", length=255, nullable=true)
-     * @Assert\Url(checkDNS=true)
+     * @Assert\Url
      */
     private $link;
 
